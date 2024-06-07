1aed7f38fb414d2888fa9161d75fda948fddf4c8
alexgithublab <alexandre@nitrokey.com>
2024-06-07 13:56:04 +0200

    fix ci

--- make-image.sh
+++ make-image.sh
@@ -42,26 +42,33 @@ if [ ! -f "${RELEASE_ISO_FILENAME}" ]; then
 fi
 
 
+modprobe loop  # This may not be necessary, depending on your kernel build but is harmless.
+major=$(grep loop /proc/devices | cut -c3)
+for index in 0 1 2 3 4 5
+do
+  mknod /dev/loop$i b $major $i
+done
+
 dd if=/dev/zero of=qubes_oem.img bs=1M count=500 
 cat "${RELEASE_ISO_FILENAME}" qubes_oem.img > $CUSTOM_ISO_FILENAME
-#DEV_QUBES_IMG="$(sudo losetup -f -P --show $CUSTOM_ISO_FILENAME)"
+DEV_QUBES_IMG="$(sudo losetup -f -P --show $CUSTOM_ISO_FILENAME)"
 #FIXME Start and End not acurate 
-echo -e "n \n\n\n\n w "|sudo  fdisk ${CUSTOM_ISO_FILENAME}
-sudo mkfs.ext4 ${CUSTOM_ISO_FILENAME}p4 
-sudo tune2fs -L QUBES_OEM ${CUSTOM_ISO_FILENAME}p4
+echo -e "n \n\n\n\n w "|sudo  fdisk ${DEV_QUBES_IMG}
+sudo mkfs.ext4 ${DEV_QUBES_IMG}p4 
+sudo tune2fs -L QUBES_OEM ${DEV_QUBES_IMG}p4
 
 if [[ -d /tmp/mnt ]] ; then
 	rm -rf /tmp/mnt
 fi	
 
 mkdir /tmp/mnt
-sudo mount ${CUSTOM_ISO_FILENAME}p4 /tmp/mnt
+sudo mount ${DEV_QUBES_IMG}p4 /tmp/mnt
 sudo cp ks.cfg /tmp/mnt
 sudo cp -r nitrokey /tmp/mnt
 sudo cp -r gpu_install /tmp/mnt
 sudo umount /tmp/mnt
-echo "write" |sudo sfdisk --wipe always ${CUSTOM_ISO_FILENAME}
-sudo losetup -d ${CUSTOM_ISO_FILENAME}
+echo "write" |sudo sfdisk --wipe always ${DEV_QUBES_IMG}
+sudo losetup -d ${DEV_QUBES_IMG}
 
 
 
